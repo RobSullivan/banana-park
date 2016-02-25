cdf7cdb7c4f6e4cde03d5e5e236bd5542bc91651
Robert Sullivan <robertjsullivan.esq@gmail.com>
2016-02-25 23:53:51 +0000

    Made changes as per tutorial.
    Added html for form and functions for controllers and added POST method.
    See google doc for minor changes to code to account for updates in the deps used in this tutorial.
    
    lein run still produces a nice looking error page which points to my sql query.

--- src/clj/banana_park/routes/home.clj
+++ src/clj/banana_park/routes/home.clj
@@ -1,17 +1,40 @@
 (ns banana-park.routes.home
   (:require [banana-park.layout :as layout]
-            [compojure.core :refer [defroutes GET]]
-            [ring.util.http-response :as response]
-            [clojure.java.io :as io]))
+            [compojure.core :refer [defroutes GET POST]]
+            [ring.util.http-response :as ok]
+            [clojure.java.io :as io]
+            [banana-park.db.core :as db]
+            [bouncer.core :as b]
+            [bouncer.validators :as v]
+            [ring.util.http-response :refer [found]]))
 
-(defn home-page []
+(defn home-page [{:keys [flash]}]
   (layout/render
-    "home.html" {:docs (-> "docs/docs.md" io/resource slurp)}))
+    "home.html"
+    (merge {:questions (db/get-questions!)}
+           (select-keys flash  [:question :answer :errors]))))
 
 (defn about-page []
   (layout/render "about.html"))
 
-(defroutes home-routes
-  (GET "/" [] (home-page))
-  (GET "/about" [] (about-page)))
 
+(defn validate-question [params]
+  (first
+    (b/validate
+      params
+      :question v/required
+      :answer v/required )))
+
+(defn save-question! [{:keys [params]}]
+  (if-let [errors (validate-question params)]
+    (-> (ok/found "/")
+        (assoc :flash (assoc params :errors errors)))
+    (do
+      (db/create-question!
+        (assoc params :timestamp (java.util.Date.)))
+      (ok/found "/"))))
+
+(defroutes home-routes
+  (GET "/" request (home-page request))
+  (POST "/" request (save-question! request))
+  (GET "/about" [] (about-page)))
